af1a1dcbdf296499b43228c7318486a1ae5ed6d8
Brad <th.lee@ecubelabs.com>
2017-03-17 21:28:46 +0900

    add compile scss, js

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,7 +6,7 @@
     <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Slabo+27px" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/css/materialize.min.css">
-    <link rel="stylesheet" type="text/css" href="{{url('css/style.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{url('css/app.css')}}">
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 </head>
